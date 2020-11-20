e58ffe5957382a0dd5d9da38d3f0d64ef8f24f64
estemax12 <69154375+estemax12@users.noreply.github.com>
2020-11-19 19:45:58 -0500

    Add files via upload

---
+++ P2T6_Variables.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("P2T6_Variables");
+
+        int num1 = 20, num2 = 25; 
+        Double num3 = 23.5656565, num4 = 5987.343243; 
+
+        System.out.println("int numbers "+num1+" . "+num2);
+        System.out.println("double number "+ num3 + " . " +num4);
+    }
+}
